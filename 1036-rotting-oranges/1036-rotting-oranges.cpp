ac85dc2e9819e11085e570b762b24b497ccf4a1c
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-03-20 22:20:39 +0530

    Time: 0 ms (100%), Space: 17.4 MB (15.11%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
-        queue<pair<int, int>> rotten;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i, j});
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        int i,j,cntFresh=0;
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j]=2;
+                } 
+                if(grid[i][j]==1) cntFresh++;
             }
         }
-        
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-        
-        while(!rotten.empty()){
-            int k = rotten.size();
-            cnt += k; 
-            while(k--){
-                int x = rotten.front().first, y = rotten.front().second;
-                rotten.pop();
-                for(int i = 0; i < 4; ++i){
-                    int nx = x + dx[i], ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
-                    grid[nx][ny] = 2;
-                    rotten.push({nx, ny});
+
+        vector<int> delRow={-1,0,1,0};
+        vector<int> delCol={0,1,0,-1};
+        int t=0; 
+        int cnt=0;
+        while(!q.empty()){
+            pair<pair<int,int>,int> val=q.front();
+            q.pop();
+            int x=val.first.first;
+            int y=val.first.second;
+            int time=val.second;
+            t=max(t,time);
+            for(i=0;i<4;i++){
+                int nRow=x+delRow[i];
+                int nCol=y+delCol[i];
+
+                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n && visited[nRow][nCol]!=2 && grid[nRow][nCol]==1){
+                    q.push({{nRow,nCol},time+1});
+                    visited[nRow][nCol]=2;
+                    cnt++;
                 }
             }
-            if(!rotten.empty()) days++;
         }
-        
-        return tot == cnt ? days : -1;
+
+        if(cnt!=cntFresh) return -1;
+        return t;
     }
 };
