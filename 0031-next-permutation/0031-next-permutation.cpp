728f1b9cc08cd876e821fe0f9eef22f8c01097e3
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-04-04 23:13:16 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int breakIdx=-1,i;
-        for(i=nums.size()-1;i>0;i--){
+        int pivot=-1,n=nums.size();
+        for(int i=n-1;i>0;i--){
             if(nums[i]>nums[i-1]){
-                breakIdx=i-1;
+                pivot=i-1;
                 break;
             }
         }
-        if(breakIdx==-1){
-            sort(nums.begin(),nums.end());
-            return;
-        }
-        for(i=nums.size()-1;i>breakIdx;i--){
-            if(nums[i]>nums[breakIdx]){
-                swap(nums[breakIdx],nums[i]);
-                break;
+        if(pivot==-1) sort(nums.begin(),nums.end());
+        else{
+            for(int i=n-1;i>pivot;i--){
+                if(nums[i]>nums[pivot]){
+                    swap(nums[i],nums[pivot]);
+                    break;
+                }
             }
+            reverse(nums.begin()+(pivot+1),nums.end());
         }
-        reverse(nums.begin()+(breakIdx+1),nums.end());
-}
-
+    }
 };
