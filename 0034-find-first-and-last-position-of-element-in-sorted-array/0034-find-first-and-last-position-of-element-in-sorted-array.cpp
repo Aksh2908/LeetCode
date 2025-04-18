77f76a86325700e8162494d0f2a1a1090f8800e4
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-04-18 15:17:06 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,44 @@
 class Solution {
-public:
+private:
     int first(vector<int>& nums, int target){
-        int low=0,high=nums.size()-1,first=-1;
+        int low=0,high=nums.size()-1;
+        int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target){
-                first=mid;
+                ans=mid;
                 high=mid-1;
             }
-            else if(nums[mid]<target)
-                low=mid+1;
-            else 
+            else if(nums[mid]>target){
                 high=mid-1;
+            }
+            else low=mid+1;
         }
-        return first;
+        return ans;
     }
-
-    int second(vector<int>& nums, int target){
-        int low=0,high=nums.size()-1,second=-1;
+    int last(vector<int>& nums, int target){
+        int low=0,high=nums.size()-1;
+        int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target){
-                second=mid;
+                ans=mid;
                 low=mid+1;
             }
-            else if(nums[mid]<target)
-                low=mid+1;
-            else 
+            else if(nums[mid]>target){
                 high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        return second;
+        return ans;
     }
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> res;
         res.push_back(first(nums,target));
-        res.push_back(second(nums,target));
+        res.push_back(last(nums,target));
         return res;
     }
 };
