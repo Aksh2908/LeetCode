aba8d507900289df5d9755cf9627f904958c21a2
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-04-18 17:11:38 +0530

    Time: 0 ms (100%), Space: 12.6 MB (32.73%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,12 +5,12 @@ public:
         if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
-        int low=1,high=n-2;
+        int low=0,high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            if(nums[mid]>nums[mid-1]) low=mid+1;
-            else high=mid-1;
+            if( mid>0 && mid<n-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            if(nums[mid]>nums[mid+1]) high=mid-1;
+            else low=mid+1;
         }
         return -1;
     }
