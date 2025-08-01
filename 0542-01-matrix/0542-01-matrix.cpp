59311d983237047ec329d40aaea682768beb9067
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-08-01 22:20:24 +0530

    Time: 31 ms (21.96%), Space: 38.7 MB (44.34%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,18 +3,18 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>> ans(m,vector<int>(n,0));
-        vector<vector<int>> dist(m,vector<int>(n,0));
 
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        vector<vector<int>> dist(m,vector<int>(n,0));
         vector<int> row={-1,0,1,0};
         vector<int> col={0,-1,0,1};
 
-        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!mat[i][j]){
+                if(mat[i][j]==0){
                     q.push({{i,j},0});
-                    ans[i][j]=1;
+                    visited[i][j]=1;
                 }
             }
         }
@@ -23,7 +23,6 @@ public:
             int i=q.front().first.first;
             int j=q.front().first.second;
             int d=q.front().second;
-
             q.pop();
 
             dist[i][j]=d;
@@ -32,13 +31,12 @@ public:
                 int ni=i+row[k];
                 int nj=j+col[k];
 
-                if(ni>=0 && ni<m && nj>=0 && nj<n && !ans[ni][nj]){
+                if(ni>=0 && ni<m && nj>=0 && nj<n && !visited[ni][nj]){
                     q.push({{ni,nj},d+1});
-                    ans[ni][nj]=1;
+                    visited[ni][nj]=1;
                 }
             }
         }
-
         return dist;
     }
 };
