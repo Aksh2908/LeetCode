ac0212c29e426afcd39de5583e30be3e06f78050
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-08-05 12:24:40 +0530

    Time: 3137 ms (5.04%), Space: 751.1 MB (5.07%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indeg(numCourses,0);
+
+        for(auto it:prerequisites){
+            indeg[it[1]]++;
+        }
+
+        queue<int> q;
+        vector<int> visited(numCourses,0);
+
+        for(int i=0;i<numCourses;i++){
+            if(!indeg[i]){
+                visited[i]=1;
+                q.push(i);
+            } 
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            for(auto it:prerequisites){
+                if(it[0]==node){
+                    indeg[it[1]]--;
+                    if(!indeg[it[1]]){
+                        q.push(it[1]);
+                        visited[it[1]]=1;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]) return false;
+        }
+        return true;
+    }
+};
