9395342165434bad3c424dc265fd6365de5742ea
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-01-31 15:20:08 +0530

    Time: 0 ms (100%), Space: 16.5 MB (69.08%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,46 +10,45 @@
  */
 class Solution {
 private:
+    ListNode* findKth(ListNode* head, int k){
+        while(head && k--){
+            head=head->next;
+        }
+        return head;
+    }
+
     ListNode* reverse(ListNode* head){
         if(!head || !head->next) return head;
-        ListNode* newHead = reverse(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = nullptr;
+        ListNode* newHead=reverse(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=nullptr;
         return newHead;
     }
-
-    ListNode* kthNode(ListNode* temp, int k){
-        while(temp && k!=1){
-            k--;
-            temp=temp->next;
-        }
-        return temp;
-    }
-
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         if(!head) return head;
-        ListNode* prevNode=nullptr,*nextNode=nullptr;
+
         ListNode* temp=head;
+
+        ListNode* dummy=new ListNode(-1);
+        ListNode* mover=dummy;
+
         while(temp){
-            ListNode* kNode = kthNode(temp,k);
-            if(!kNode){
-                prevNode->next = temp;
+            ListNode* kNode=findKth(temp,k-1);
+            if(kNode==nullptr){
+                mover->next=temp;
                 break;
             }
-            nextNode = kNode->next;
+            ListNode* nextNode=kNode->next;
             kNode->next=nullptr;
-            ListNode* newHead = reverse(temp);
-            if(temp==head){
-                head=newHead;
-            }
-            else{
-                prevNode->next=newHead;
-            }
-            prevNode=temp;
+            
+            mover->next=reverse(temp);
+            mover=temp;
+
             temp=nextNode;
         }
-        return head;
+
+        return dummy->next;
     }
 };
